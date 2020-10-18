a81b65921056f21c510e9f60b16a76bca12b55c1
Nathachai Thongniran <inid3a@gmail.com>
2020-10-18 20:30:32 +0700

    :sparkles: (cli) add cli support

--- wordcookies/__main__.py
+++ wordcookies/__main__.py
@@ -1,6 +1,12 @@
+import sys
+from wordcookies import cli
+
 USAGE = """wordcookies
 Usage:
 ------
+    $ wordcookies cli
+    $ wordcookies cli --word="word"
+    $ wordcookies cli --word="word" --exit
 
 Available options are:
     -h, --help         Show this help
@@ -8,7 +14,32 @@ Available options are:
 
 
 def main():
-    print("echo from cli of wordcookies module")
+    if sys.argv[1] == "cli" and len(sys.argv) >= 2:
+        args = sys.argv[2:]
+
+        word = ""
+        is_exit = False
+
+        # parsing
+        for arg in args:
+            if arg.startswith("--word"):
+                word = str(arg.split("=", 1)[1])
+            elif arg.startswith("--exit"):
+                is_exit = True
+            else:
+                print("you are passing invalid argument", arg)
+                sys.exit(0)
+
+        if word:
+            cli.proceed(word)
+            if not is_exit:
+                cli.main()
+        else:
+            cli.main()
+
+    else:
+        print(USAGE)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
--- wordcookies/cli.py
+++ wordcookies/cli.py
@@ -1,15 +1,25 @@
 from wordcookies import game
 from pprint import pprint
 
-while True:
-    try:
-        s = input("\nenter chars: ")
-        chars = game.clean(s)
-        print(chars)
-        answers = game.get_possible_answers(chars)
-        g = game.group(answers)
-        pprint(g, width=80)
 
-    except KeyboardInterrupt:
-        print("\nbye !")
-        exit(0)
+def proceed(word: str) -> None:
+    chars = game.clean(word)
+    print(chars)
+    answers = game.get_possible_answers(chars)
+    g = game.group(answers)
+    pprint(g, width=80)
+
+
+def main():
+    while True:
+        try:
+            word = input("\nenter chars: ")
+            proceed(word)
+
+        except KeyboardInterrupt:
+            print("\nbye !")
+            exit(0)
+
+
+if __name__ == '__main__':
+    main()
