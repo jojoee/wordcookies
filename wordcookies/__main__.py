3348a5771126046b1331f97f6208a9bc7af6ac9c
Nathachai Thongniran <inid3a@gmail.com>
2020-10-18 18:46:20 +0700

    :sparkles: (init) add __init__.py and __main__.py

---
+++ wordcookies/__main__.py
@@ -0,0 +1,15 @@
+USAGE = """wordcookies
+Usage:
+------
+
+Available options are:
+    -h, --help         Show this help
+"""
+
+
+def main():
+    print("echo from cli of wordcookies module")
+
+
+if __name__ == "__main__":
+    main()
