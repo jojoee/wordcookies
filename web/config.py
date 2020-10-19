17a408373ea0ca6e1a201ad49d63b9f931426c57
Nathachai Thongniran <inid3a@gmail.com>
2020-10-19 22:01:36 +0700

    :sparkles: (cache) support both Redis and file system

--- web/config.py
+++ web/config.py
@@ -11,3 +11,9 @@ load_dotenv(dotenv_path=dotenv_path, verbose=True)
 # app
 IS_DEBUG = bool(os.getenv("IS_DEBUG")) if os.getenv("IS_DEBUG") else False
 PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 9001
+
+# REDIS
+REDIS_HOST = os.getenv("REDIS_HOST") or ""
+REDIS_PASS = os.getenv("REDIS_PASS") or ""
+REDIS_PORT = int(os.getenv("REDIS_PORT")) if os.getenv("REDIS_PORT") else 6379
+REDIS_DB = int(os.getenv("REDIS_DB")) if os.getenv("REDIS_DB") else 0
--- web/main.py
+++ web/main.py
@@ -1,13 +1,28 @@
+import json
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse  # Use this to serve a public/index.html
-from config import IS_DEBUG, PORT
+from simplekv.memory.redisstore import RedisStore
+from simplekv.fs import FilesystemStore
+import redis
+from config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASS, IS_DEBUG, PORT
 import uvicorn
 from wordcookies import game
 
 # init FastAPI
 app = FastAPI()
 app.mount("/public", StaticFiles(directory="./public"), name="public")
+
+# init cache
+# TODO: move to constant
+text_domain = "wordcookies"
+if REDIS_HOST == "":
+    cache_path = "./cache"
+    print("cache storage: file, inside %s" % cache_path)
+    store = FilesystemStore(cache_path)
+else:
+    print("cache storage: Redis, host %s" % REDIS_HOST)
+    store = RedisStore(redis.StrictRedis(REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASS))
 
 
 @app.get("/")
@@ -27,10 +42,24 @@ def read_404():
 
 @app.get("/api/solve/{s}")
 def read_input(s: str):
-    chars = game.clean(s)
+    global text_domain
 
-    answers = game.get_possible_answers(chars)
-    g = game.group(answers)
+    chars = game.clean(s)
+    key_name = "%s_%s" % (text_domain, "".join(chars))
+    g = None
+
+    try:
+        cached_data = store.get(key_name)
+        print("key_name: %s, found cached_data" % key_name)
+        g = json.loads(cached_data.decode("utf8").replace("'", '"'))
+
+    except Exception as e:
+        print("key_name: %s, not found cached_data" % str(e))
+        answers = game.get_possible_answers(chars)
+        g = game.group(answers)
+
+        # save cache
+        store.put(key_name, json.dumps(g).encode("utf-8"))
 
     return {"code": 200, "message": "", "data": g}
 
