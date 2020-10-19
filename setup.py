dd1f459738491b52c4c631e1f4647d44b311bf80
Nathachai Thongniran <inid3a@gmail.com>
2020-10-19 21:24:36 +0700

    :bookmark: (1.0.0) first release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="wordcookies",
-    version="0.0.1",
+    version="1.0.0",
     description="Word Cookies helper/solver/answers",
     long_description=README,
     long_description_content_type="text/markdown",
